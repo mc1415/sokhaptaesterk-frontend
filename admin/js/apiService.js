38753db657d9b068713f0f09e253d448351eab30
mc1415 <chhorngcia@gmail.com>
2025-06-14 14:07:27 +0700

    Update API

--- admin/js/apiService.js
+++ admin/js/apiService.js
@@ -1,5 +1,5 @@
 // frontend/admin/js/apiService.js
-const API_BASE_URL = 'http://localhost:10000/api'; 
+const API_BASE_URL = 'https://sokhaptaesterk-backend.onrender.com/api'; 
 
 async function apiFetch(endpoint, options = {}) {
     const token = localStorage.getItem('authToken');
